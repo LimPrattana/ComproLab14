3c87ae149322beb905600bb14ee50784be84eec8
LimPrattana <vivo.twy0270@gmail.com>
2025-01-28 22:30:28 +0700

    lab14_1 com

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
 using namespace std;
 
+/*template <typename T>
+void insertionSort(T d[],int N){
+	for(int end=N;end>1;end--){
+		cout<<"Pass "<<N-end+1<<":";
+		for(int i=0;i<N-1;i++){
+			for(int k=0;k<i-1;k++){
+				if(d[k]<d[k+1]) {
+				T temp = d[k];
+				d[k]=d[k+1];
+				d[k+1]=temp;
+				}
+			}
+			
+	}
+	for(int j=0;j<N;j++){
+			cout<<d[j]<<" ";
+		}
+	cout<<endl;
+	}
+}*/
 template <typename T>
 void insertionSort(T d[],int N){
-
+	 for(int i=1;i<N;i++){
+		T s =d[i];
+		
+		int j = i-1;
+		while(j>=0&&d[j]<s)
+		{
+			d[j+1]=d[j];
+			j--;
+		}
+		d[j+1]=s;
+		cout<<"Pass "<<i<<":";
+		for(int k=0;k<N;k++){
+			cout<<d[k]<<" ";
+		}
+	cout<<endl;
+		
+	 }
 }
 
 int main(){
